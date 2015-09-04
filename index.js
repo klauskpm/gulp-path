6d4446a8b0828c06f69f46f7ad47981104cb814c
= <klausmachado@gmail.com>
2015-09-04 13:14:08 -0300

    2.0.0: chances the base way to call it and the Path class;

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-module.exports = function (opt) {
-    var basePaths = {
-        dest: opt.dest,
-        src: opt.src
-    };
+module.exports = {
+    Base: function (opt) {
 
-    return {
-        basePaths: basePaths,
-        Asset: function (path, extension) {
-            extension = (typeof extension === "undefined" ? "" : "." + extension);
-
-            this.dest = basePaths.dest + path + "/";
-            this.src = basePaths.src + path + "/**/*" + extension;
+        if (typeof opt === "undefined" || !opt) {
+            opt = {
+                dest: "./",
+                src: "./"
+            };
+        } else if (typeof opt === "string") {
+            opt = {
+                dest: opt,
+                src: opt
+            };
         }
-    };
+
+        var basePaths = {
+            dest: opt.dest,
+            src: opt.src
+        };
+
+        return {
+            basePaths: basePaths,
+            Path: function (path, extension) {
+                extension = (typeof extension === "undefined" ? "" : "." + extension);
+                fullPath = basePaths.dest + path + "/";
+
+                this.dest = fullPath;
+                this.src = fullPath + "**/*" + extension;
+                this.files = function (files, filesExtension) {
+                    filesExtension = (typeof filesExtension === "undefined" ? "" : "." + filesExtension);
+                    if (typeof files === "undefined") {
+                        return false;
+                    } else if (typeof files === "string") {
+                        files = [files];
+                    }
+
+                    var paths = [];
+                    files.forEach( function (file) {
+                        paths.push(fullPath + file + filesExtension);
+                    });
+
+                    return paths;
+                }
+            }
+        };
+    }
 };
