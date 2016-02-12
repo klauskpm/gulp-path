297d77bb7158e8cf98e36a6b9c729b22e2a3055a
Klaus Kazlauskas <klausmachado@concursovirtual.com.br>
2016-02-12 15:31:48 -0200

    V3.0.1 bugs fixes

--- index.js
+++ index.js
@@ -3,44 +3,6 @@
 var Base = require('./base');
 var filesPaths = require('./files');
 
-var assets = new Base({
-        src: './assets/',
-        dest: './resources/'
-    }),
-    views = new Base('./views/'),
-    root = new Base(),
-    app = new Base({
-        src: [
-            './app/',
-            './js/'
-        ],
-        dest: [
-            './dest/',
-            './concat/'
-        ]
-    });
-
-var paths = {
-        img: assets.Path('img'),
-        js: assets.Path('js', 'js'),
-        fonts: views.Path('fonts'),
-        controllers: root.Path('controllers', 'js'),
-        data: app.Path()
-    };
-
-console.log(paths);
-
-console.log(
-    paths.img.files(null, ['png', 'jpg', 'gif']),
-    paths.img.files(),
-    paths.js.files(['compiled', 'main'], 'min.js'),
-    paths.fonts.files(['Roboto', 'glyphicons-halflings-regular'], ['eot', 'ttf', 'svg', 'woff', 'woff2']),
-    paths.controllers.files('homeController', ['min.js', 'js']),
-    paths.data.files(['main', 'sub-service'], ['min.js', 'js']),
-    filesPaths(['./app/', './resource/**/'], null, ['scss', 'css']),
-    filesPaths()
-);
-
 module.exports = {
     Base: Base,
     filesPaths: filesPaths
